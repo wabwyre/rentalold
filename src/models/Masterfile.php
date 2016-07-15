718d426aaae19385f6283f9ac45fd6efcb61891b
joel <joelmuindi@gmail.com>
2016-07-15 13:26:22 +0300

    bank module

--- src/models/Masterfile.php
+++ src/models/Masterfile.php
@@ -186,6 +186,20 @@
                 'plot' => array(
                     'name' => 'Plot',
                     'required' => true
+                ),
+                'account_no' => array(
+                    'name' => 'Account Number',
+                    'required' => true
+                ),
+                'bank_name' => array(
+                    'name' => 'Bank Name',
+                    'unique' => 'banks',
+                    'required' => true
+                ),
+                'branch_name' => array(
+                    'name' => 'Branch Name',
+                    'unique' => 'bank_branch',
+                    'required' => true
                 )
             ));
 
@@ -204,7 +218,7 @@
                 $mf_id = $this->addPersonalDetails($surname, $firstname, $middlename, $id_passport, $gender, $image_path, $regdate_stamp, $b_role, $customer_type_id, $email);
                 if(!empty($mf_id)){
                     if($this->attachPlotToLandlord($mf_id, $plot)){
-                        if($this->attachbankToLandlord($mf_id, $bank_id, $branch_id, $account_no)){
+                        if($this->attachbankToLandlord($mf_id, $bank_name, $branch_name, $account_no)){
                             if($this->addAddress($phone_number, $postal_address, $town, $mf_id, $address_type_id, $ward, $street, $building, $county, $postal_code)) {
                                 if($this->createLoginAccount($landlord_data, $mf_id)) {
                                     $this->endTranc();
@@ -453,30 +467,12 @@
 			}
 		}
 
-        public function getAllBank($condition = null){
-            $condition = (!is_null($condition)) ? $condition : '';
-            $data = $this->selectQuery('banks', '*', $condition);
-            return array(
-                'all' => $data,
-                'specific' => $data[0]
-            );
-        }
-
-        public function getAllBranch($condition = null){
-            $condition = (!is_null($condition)) ? $condition : '';
-            $data = $this->selectQuery('bank_and_branches', '*', $condition);
-            return array(
-                'all' => $data,
-                'specific' => $data[0]
-            );
-        }
-
-        public function attachbankToLandlord($mf_id, $bank_id, $branch_id, $account_no){
+        public function attachbankToLandlord($mf_id, $bank_name, $branch_name, $account_no){
             $result = $this->insertQuery(
                 'bank_account',
                 array(
-                    'bank_id' => $bank_id,
-                    'branch_id' => $branch_id,
+                    'bank_name' => $bank_name,
+                    'branch_name' => $branch_name,
                     'account_no' => $account_no,
                     'mf_id' => $mf_id
                 )
